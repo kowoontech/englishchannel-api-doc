19a26ee2d184607d838a6865722c71243e72207e
박배식 커머스공통개발(YA) <baesik.park@yanolja.com>
2024-05-05 19:54:36 +0900

    공통 옵션 목록 조회

--- docs/api_project.js
+++ docs/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-05-05T10:41:44.897Z",
+    "time": "2024-05-05T10:54:19.480Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
