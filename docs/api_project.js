3b5585385cf12b6c6b79e6f9b80924cccdb39992
gyj <jinyjgo@gmail.com>
2024-06-24 14:57:55 +0900

    비고 수정/ 삭제 api

--- docs/api_project.js
+++ docs/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-06-23T09:21:48.594Z",
+    "time": "2024-06-24T05:57:35.540Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
---
+++ src/1_admin-api/01_user/27_update_user_notes.js
@@ -0,0 +1,23 @@
+/**
+ * @api {put} /admin/v1/users/note/{id} 27. 비고 수정
+ *
+ * @apiDescription 회원관리 > 기본 탭 > 예약 > 예약등록 > TIP
+ *
+ * @apiVersion 1.0.0
+ * @apiName admin_updateUser
+ * @apiGroup 1. Admin API > 1. 회원
+ *
+ * @apiExample {curl} REQUEST
+ * curl -i -X PUT 'http://localhost:8080/admin/v1/users/notes/2666892' \
+ * -H "Content-Type: application/json" \
+ * -d "{
+           "content" : "수정"
+       }"
+ *
+ * @apiParam (Path) {Long} [id] 비고식별키
+ * 
+ * @apiParam (Body) {String} [content] 비고 내용
+ *
+ * @apiSuccessExample {json} SUCCESS
+ * HTTP/1.1 200
+ */
