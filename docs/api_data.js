eeeb64c9d5b99e835ddace9e01fff1cd443c366b
gyj <jinyjgo@gmail.com>
2024-06-24 19:29:30 +0900

    상담고객상세조회

--- docs/api_data.js
+++ docs/api_data.js
@@ -85,6 +85,287 @@ define({ "api": [
     "groupTitle": "1._Admin_API_>_0._공통_API"
   },
   {
+    "type": "get",
+    "url": "/admin/v1/consultations/{id}",
+    "title": "03. 상담고객 상세조회",
+    "description": "<p>수정/삭제 시 사용<br/></p>",
+    "version": "1.0.0",
+    "name": "admin_getOptions",
+    "group": "1._Admin_API_>_0._공통_API",
+    "examples": [
+      {
+        "title": "REQUEST",
+        "content": "curl -i -X GET 'http://localhost:8080/admin/v1/consultations/14890'",
+        "type": "curl"
+      }
+    ],
+    "parameter": {
+      "fields": {
+        "Query": [
+          {
+            "group": "Query",
+            "optional": false,
+            "field": "id",
+            "description": "<p>상담 식별키</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": true,
+            "field": "consultation",
+            "description": "<p>상담 정보</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.id",
+            "description": "<p>상담 식별키</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.institutionId",
+            "description": "<p>추천인 식별키</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.consultationDate",
+            "description": "<p>추천인 식별키</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.name",
+            "description": "<p>성명</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.gender",
+            "description": "<p>성별 [남:&quot;M&quot;,여:&quot;F&quot;]</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.job",
+            "description": "<p>직업</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.company",
+            "description": "<p>회사명</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.phone",
+            "description": "<p>번호</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.cellPhone",
+            "description": "<p>핸드폰번호</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.foundPath",
+            "description": "<p>알게된 동기<br/> SIGN(&quot;10&quot;), // 간판<br/> ONLINE(&quot;20&quot;), // 온라인검색<br/> RECOMMEND(&quot;30&quot;), // 지인추천<br/> FAN(&quot;40&quot;), // 부채<br/> LEAFLET(&quot;50&quot;), // 3단리플릿<br/> SUBWAY(&quot;60&quot;), // 지하철광고<br/> ALLIANCE(&quot;70&quot;), // 기업제휴<br/> ETC(&quot;900&quot;), // 기타<br/></p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.foundPathNote",
+            "description": "<p>알게된 동기 설명</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.visitDate",
+            "description": "<p>방문일자</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.details",
+            "description": "<p>상담내용</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.isMember",
+            "description": "<p>회원여부</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.type",
+            "description": "<p>상담구분 [  P, // 전화상담<br> V, // 방문상담<br> K, // 카카오채널<br> N, // 네이버예약<br> F, // 온라인상담<br> L, // 무료레벨테스트]</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.studyPurpose",
+            "description": "<p>공부목적[  COMMON_ENGLISH(&quot;10&quot;), // 생활영어<br/> WORK(&quot;20&quot;), //업무<br/> STUDY_ABROAD(&quot;30&quot;), // 유학<br/> DEVELOPMENT(&quot;40&quot;), // 자기개<br/> EMPLOYMENT(&quot;50&quot;), // 취업<br/> ETC(&quot;60&quot;) // 기타]</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.callTime",
+            "description": "<p>통화가능시간 [  TIME_01(&quot;10&quot;), // 06:30~08:00<br/> TIME_02(&quot;20&quot;), // 08:00~10:00<br/> TIME_03(&quot;30&quot;), // 10:00~12:00<br/> TIME_04(&quot;40&quot;), // 12:00~14:00<br/> TIME_05(&quot;50&quot;), // 14:00~16:00<br/> TIME_06(&quot;60&quot;), // 16:00~18:00<br/> TIME_07(&quot;70&quot;), // 18:00~20:00<br/> TIME_08(&quot;80&quot;), // 20:00~21:00<br/> TIME_09(&quot;90&quot;), // 기타]<br/></p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.status",
+            "description": "<p>처리상태 WAITING(&quot;3&quot;), // 대기<br/> NO_CONTACT(&quot;4&quot;),<br/> UNVISITED(&quot;5&quot;),<br/> RESERVED(&quot;6&quot;),<br/> NOT_REGISTERED(&quot;7&quot;),<br/> REGISTERED(&quot;8&quot;),<br/> NO_SHOW(&quot;9&quot;),<br/> MISSED(&quot;10&quot;),<br/></p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.statuses",
+            "description": "<p>사용X</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.levelTestType",
+            "description": "<p>[R 밖에 없음.]</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.levelTestAnswer",
+            "description": "<p>사용X</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.levelTestCorrectCount",
+            "description": "<p>정답수</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.createdOn",
+            "description": "<p>등록일 [yyyy-MM-dd HH:mm:ss]</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.createdBy",
+            "description": "<p>등록한사람ID</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.modifiedBy",
+            "description": "<p>수정한사람ID [yyyy-MM-dd HH:mm:ss]</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.modifiedOn",
+            "description": "<p>수정일 [yyyy-MM-dd HH:mm:ss]</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "consultation.creatorName",
+            "description": "<p>사용X</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "createdName",
+            "description": "<p>상담자(등록한사람이름)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "modifiedName",
+            "description": "<p>상담자(수정한사람이름)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": true,
+            "field": "histories",
+            "description": "<p>추가상담이력</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "histories.id",
+            "description": "<p>추가상담이력 식별키</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "histories.date",
+            "description": "<p>사용여부미정</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "histories.details",
+            "description": "<p>상담내용</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "SUCCESS",
+          "content": "HTTP/1.1 200\n{\n    \"consultation\": {\n        \"id\": 14890,\n        \"institutionId\": \"C1352081511487906\",\n        \"consultationDate\": \"2020-07-28 14:00:00\",\n        \"name\": \"이우정:상담완료\",\n        \"gender\": \"M\",\n        \"job\": \"직장인\",\n        \"company\": \"(주)후이즈\",\n        \"phone\": null,\n        \"cellPhone\": \"5gz14POxRDFNoPlTCEGupw==\",\n        \"foundPath\": \"20\",\n        \"foundPathNote\": \"워크인\",\n        \"visitDate\": \"2020-07-28T14:00:00\",\n        \"details\": \"* 워크인,이신종님과 같이옴 * 해외시장확장으로 전반적 영어실력향상 원함 * 점심시간에 잠시 나오신거라 레벨테스트까지 할시간없어  정규코스, 5%할인까지 안내드림 * 큐브정규코스, 온라인학습 안내드림 * 가격듣고는 일단 알겠다고 바로나가심\",\n        \"isMember\": \"N\",\n        \"type\": \"V\",\n        \"studyPurpose\": \"10\",\n        \"etcStudyPurpose\": null,\n        \"callTime\": \"90\",\n        \"email\": \"010-3790-2702\",\n        \"status\": \"7\",\n        \"statuses\": null,\n        \"levelTestType\": null,\n        \"levelTestAnswer\": null,\n        \"levelTestCorrectCount\": null,\n        \"createdBy\": \"U1585270523092843\",\n        \"createdOn\": \"2020-07-28 14:23:41\",\n        \"modifiedBy\": \"U1585270523092843\",\n        \"modifiedOn\": \"2020-07-28\",\n        \"creatorName\": null\n    },\n    \"createdName\": \"양은진\",\n    \"modifiedName\": \"양은진\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "src/1_admin-api/04.consultation/03_get_consultation.js",
+    "groupTitle": "1._Admin_API_>_0._공통_API"
+  },
+  {
     "type": "post",
     "url": "/admin/v1/login",
     "title": "02. 로그인",
---
+++ src/1_admin-api/04.consultation/03_get_consultation.js
@@ -0,0 +1,124 @@
+/**
+ * @api {get} /admin/v1/consultations/{id} 03. 상담고객 상세조회
+ *
+ * @apiDescription 수정/삭제 시 사용<br/>
+ *
+ * @apiVersion 1.0.0
+ * @apiName admin_getOptions
+ * @apiGroup 1. Admin API > 0. 공통 API
+ *
+ * @apiExample {curl} REQUEST
+ * curl -i -X GET 'http://localhost:8080/admin/v1/consultations/14890'
+ *
+ * @apiParam (Query) id 상담 식별키
+ *
+ * @apiSuccess {Object[]} [consultation] 상담 정보
+ * @apiSuccess {String} consultation.id 상담 식별키
+ * @apiSuccess {String} consultation.institutionId 추천인 식별키
+ * @apiSuccess {String} consultation.consultationDate 추천인 식별키
+ * @apiSuccess {String} consultation.name 성명
+ * @apiSuccess {String} consultation.gender 성별 [남:"M",여:"F"]
+ * @apiSuccess {String} consultation.job 직업
+ * @apiSuccess {String} consultation.company 회사명
+ * @apiSuccess {String} consultation.phone 번호
+ * @apiSuccess {String} consultation.cellPhone 핸드폰번호
+ * @apiSuccess {String} consultation.foundPath 알게된 동기<br/>
+                                                    SIGN("10"), // 간판<br/>
+                                                    ONLINE("20"), // 온라인검색<br/>
+                                                    RECOMMEND("30"), // 지인추천<br/>
+                                                    FAN("40"), // 부채<br/>
+                                                    LEAFLET("50"), // 3단리플릿<br/>
+                                                    SUBWAY("60"), // 지하철광고<br/>
+                                                    ALLIANCE("70"), // 기업제휴<br/>
+                                                    ETC("900"), // 기타<br/>
+ * @apiSuccess {String} consultation.foundPathNote 알게된 동기 설명
+ * @apiSuccess {String} consultation.visitDate 방문일자
+ * @apiSuccess {String} consultation.details 상담내용
+ * @apiSuccess {String} consultation.isMember 회원여부
+ * @apiSuccess {String} consultation.type 상담구분 [  P, // 전화상담<br>
+                                              V, // 방문상담<br>
+                                              K, // 카카오채널<br>
+                                              N, // 네이버예약<br>
+                                              F, // 온라인상담<br>
+                                              L, // 무료레벨테스트]
+ * @apiSuccess {String} consultation.studyPurpose 공부목적[  COMMON_ENGLISH("10"), // 생활영어<br/>
+                                                         WORK("20"), //업무<br/>
+                                                         STUDY_ABROAD("30"), // 유학<br/>
+                                                         DEVELOPMENT("40"), // 자기개<br/>
+                                                         EMPLOYMENT("50"), // 취업<br/>
+                                                         ETC("60") // 기타]
+ * @apiSuccess {String} consultation.callTime 통화가능시간 [  TIME_01("10"), // 06:30~08:00<br/>
+                                                        TIME_02("20"), // 08:00~10:00<br/>
+                                                        TIME_03("30"), // 10:00~12:00<br/>
+                                                        TIME_04("40"), // 12:00~14:00<br/>
+                                                        TIME_05("50"), // 14:00~16:00<br/>
+                                                        TIME_06("60"), // 16:00~18:00<br/>
+                                                        TIME_07("70"), // 18:00~20:00<br/>
+                                                        TIME_08("80"), // 20:00~21:00<br/>
+                                                        TIME_09("90"), // 기타]<br/>
+ * @apiSuccess {String} consultation.status 처리상태 WAITING("3"), // 대기<br/>
+                                                   NO_CONTACT("4"),<br/>
+                                                   UNVISITED("5"),<br/>
+                                                   RESERVED("6"),<br/>
+                                                   NOT_REGISTERED("7"),<br/>
+                                                   REGISTERED("8"),<br/>
+                                                   NO_SHOW("9"),<br/>
+                                                   MISSED("10"),<br/>
+ * @apiSuccess {String} consultation.statuses 사용X
+ * @apiSuccess {String} consultation.levelTestType  [R 밖에 없음.]
+ * @apiSuccess {String} consultation.levelTestAnswer 사용X
+ * @apiSuccess {String} consultation.levelTestCorrectCount 정답수
+ * @apiSuccess {String} consultation.createdOn 등록일 [yyyy-MM-dd HH:mm:ss]
+ * @apiSuccess {String} consultation.createdBy 등록한사람ID
+ * @apiSuccess {String} consultation.modifiedBy 수정한사람ID [yyyy-MM-dd HH:mm:ss]
+ * @apiSuccess {String} consultation.modifiedOn 수정일 [yyyy-MM-dd HH:mm:ss]
+ * @apiSuccess {String} consultation.details 상담내용
+ * @apiSuccess {String} consultation.creatorName 사용X
+
+ * @apiSuccess {String} createdName 상담자(등록한사람이름)
+ * @apiSuccess {String} modifiedName 상담자(수정한사람이름)
+
+ * @apiSuccess {Object[]} [histories] 추가상담이력
+ * @apiSuccess {String} histories.id 추가상담이력 식별키
+ * @apiSuccess {String} histories.date 사용여부미정
+ * @apiSuccess {String} histories.details 상담내용
+ * @apiSuccess {String} histories.date 사용여부미정
+ *
+ * @apiSuccessExample {json} SUCCESS
+ * HTTP/1.1 200
+{
+    "consultation": {
+        "id": 14890,
+        "institutionId": "C1352081511487906",
+        "consultationDate": "2020-07-28 14:00:00",
+        "name": "이우정:상담완료",
+        "gender": "M",
+        "job": "직장인",
+        "company": "(주)후이즈",
+        "phone": null,
+        "cellPhone": "5gz14POxRDFNoPlTCEGupw==",
+        "foundPath": "20",
+        "foundPathNote": "워크인",
+        "visitDate": "2020-07-28T14:00:00",
+        "details": "* 워크인,이신종님과 같이옴 * 해외시장확장으로 전반적 영어실력향상 원함 * 점심시간에 잠시 나오신거라 레벨테스트까지 할시간없어  정규코스, 5%할인까지 안내드림 * 큐브정규코스, 온라인학습 안내드림 * 가격듣고는 일단 알겠다고 바로나가심",
+        "isMember": "N",
+        "type": "V",
+        "studyPurpose": "10",
+        "etcStudyPurpose": null,
+        "callTime": "90",
+        "email": "010-3790-2702",
+        "status": "7",
+        "statuses": null,
+        "levelTestType": null,
+        "levelTestAnswer": null,
+        "levelTestCorrectCount": null,
+        "createdBy": "U1585270523092843",
+        "createdOn": "2020-07-28 14:23:41",
+        "modifiedBy": "U1585270523092843",
+        "modifiedOn": "2020-07-28",
+        "creatorName": null
+    },
+    "createdName": "양은진",
+    "modifiedName": "양은진"
+}
+ */
