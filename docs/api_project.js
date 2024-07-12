11bc58216d315f38cfae58ea786eeaca088334d4
gyj <jinyjgo@gmail.com>
2024-07-12 09:45:07 +0900

    오타 수정

--- docs/api_project.js
+++ docs/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-07-11T08:15:17.927Z",
+    "time": "2024-07-12T00:44:50.912Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
