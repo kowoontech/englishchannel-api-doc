3e20233771355c3b6173f1b285cad2f917bef34e
gyj <jinyjgo@gmail.com>
2024-07-24 13:44:12 +0900

    상담 목록 조회 api 수정

--- docs/api_project.js
+++ docs/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-07-22T02:49:49.048Z",
+    "time": "2024-07-24T04:32:26.632Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
