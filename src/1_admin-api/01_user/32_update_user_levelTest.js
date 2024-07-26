7c1dff157ffe1a2ac6d5ed1724e844906c1c84bb
gyj <jinyjgo@gmail.com>
2024-07-26 13:26:30 +0900

    테스트 수정 설명 수정

--- src/1_admin-api/01_user/32_update_user_levelTest.js
+++ src/1_admin-api/01_user/32_update_user_levelTest.js
@@ -49,7 +49,7 @@
  * @apiParam (Body) {String} note 특이사항
  * @apiParam (Body) {String} purpose Purpose of Study
  * @apiParam (Body) {String[]} studyType What kind of English do you want to study and learn?<br/>
-                                         [NONE: 선택 없음 , EC: English Conversation,<br/>
+                                         [EC: English Conversation,<br/>
                                          BE: Business English ,TS:Toeic Speaking , I:Interview ,ETC: ETC]<br>
  * @apiParam (Body) {String} studyType ETC 이유
  * @apiParam (Body) {String} familyBackground Family Background
@@ -59,8 +59,8 @@
  * @apiParam (Body) {String} Travel Abroad
  * @apiParam (Body) {String} futurePlans Future Plans
  * @apiParam (Body) {String[]} consonants consonants <br/>
-                                        [NONE : 선택 안함, R , L, P, F, B, V, Z, SH, EZH, CH, G, TTH, TH]<br/>
- * @apiParam (Body) {String[]} vowels vowels [NONE:선택안함, A,B,C,D,E,F,G,H,I,J,K,L,M,N]
+                                        [ R , L, P, F, B, V, Z, SH, EZH, CH, G, TTH, TH]<br/>
+ * @apiParam (Body) {String[]} vowels vowels [ 순서대로 > A,B,C,D,E,F,G,H,I,J,K,L,M,N]
  * @apiParam (Body) {String} clarity Clarity(10:hard to understand ,20:average ,30:easy to understand)
  * @apiParam (Body) {String} intonation Intonation(10:very Korean ,20:a bit Korean,30:acceptable ,40:near native)
  * @apiParam (Body) {String} vocabulary Vocabulary(10:very limited ,20:limited ,30:average ,40:above average ,50:extensive)
@@ -76,7 +76,7 @@
                                             (10:almost nothing ,20:some parts ,30:most parts ,40:almost everything ,50:everything)
 
  * @apiParam (Body) {String} confidence Confidence(10:completely lacking ,20:lacking ,30:average ,40:above average,50:very confident)
- * @apiParam (Body) {String[]} recommendedLevel Recommended Level [NONE(선택안함), R2, R3,R4,R5,R6,R6,R7,R8,R9,ETC]
+ * @apiParam (Body) {String[]} recommendedLevel Recommended Level [ R2:Pre// R3:300// R4:400//R5:500 //R6:600//R7:700://R8: 800//R9:900//ETC:ETC]
  * @apiParam (Body) {String} recommendedLevelEtc Etc 이유
  *
  * @apiSuccessExample {json} SUCCESS
