46a096b740445d24b667386fe6fab908c6c4f15b
박배식 커머스공통개발(YA) <baesik.park@yanolja.com>
2024-07-29 03:20:14 +0900

    44. 회원 주문 결제 수정

--- src/1_admin-api/01_user/44_update_user_payment.js
+++ src/1_admin-api/01_user/44_update_user_payment.js
@@ -1,5 +1,5 @@
 /**
- * @api {put} /admin/v1/users/{id}/orders/{orderId}/payments/paymentId 44. 회원 주문 결제 수정
+ * @api {put} /admin/v1/users/{id}/orders/{orderId}/payments/{paymentId} 44. 회원 주문 결제 수정
  *
  * @apiDescription 회원관리 > 회원 목록 조회 > 주문 탭
  *
@@ -10,7 +10,13 @@
  * @apiExample {curl} REQUEST
  * curl -i -X PUT 'http://localhost:8080/admin/v1/users/M1722002661204896/orders/O1722163996604360/payments/P1722166085902400' \
  * -H "Content-Type: application/json" \
- * -d ""
+ * -d "{
+ *     "modifiedBy": "U1370839971594082",
+ *     "cardCompany": "NH",
+ *     "cardNumber": "11111",
+ *     "installmentMonths": null,
+ *     "approvalNumber": "22222"
+ * }"
  *
  * @apiParam (Path) {String} id 회원 식별키
  * @apiParam (Path) {String} orderId 주문 식별키
