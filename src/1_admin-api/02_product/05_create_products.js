8e7aff1346c0a6b9cd23050921eae9816d9a8ac0
gyj <jinyjgo@gmail.com>
2024-08-01 16:53:43 +0900

    상품 등록/ 수정

---
+++ src/1_admin-api/02_product/05_create_products.js
@@ -0,0 +1,26 @@
+/**
+ * @api {post} admin/v1/products 05. 상품 등록
+ *
+ * @apiDescription 상품관리 > 상품등록
+ *
+ * @apiVersion 1.0.0
+ * @apiName admin_createUserOrderRefund
+ * @apiGroup 1. Admin API > 2. 상품
+ *
+ * @apiExample {curl} REQUEST
+ * curl -i -X POST 'http://localhost:8080/admin/v1/products' \
+ * -H "Content-Type: application/json" \
+ * -d "{
+           "type": "과정",
+           "name": "패키지1",
+           "price": 13000
+       }"
+ *
+ *
+ * @apiParam (Body) {String} type 과정여부 (체크박스 선택 시 '과정'/ 선택하지 않을 시 null)
+ * @apiParam (Body) {String} name 상품명
+ * @apiParam (Body) {Number} price 가격
+ *
+ * @apiSuccessExample {json} SUCCESS
+ * HTTP/1.1 200
+ */
---
+++ src/1_admin-api/02_product/06_update_products.js
@@ -0,0 +1,25 @@
+/**
+ * @api {put} /admin/v1/products/{id} 06. 상품 수정
+ *
+ * @apiDescription 상품관리 > 수정
+ *
+ * @apiVersion 1.0.0
+ * @apiName admin_updateUserOrderPayment
+ * @apiGroup 1. Admin API > 2. 상품
+ *
+ * @apiExample {curl} REQUEST
+ * curl -i -X PUT 'http://localhost:8080/admin/v1/products/P1722475920322505' \
+ * -H "Content-Type: application/json" \
+ * -d "{
+           "type": "과정",
+           "name": "패키지1",
+           "price": 13000
+       }"
+ *
+ * @apiParam (Body) {String} type 과정여부 (체크박스 선택 시 '과정'/ 선택하지 않을 시 null)
+ * @apiParam (Body) {String} name 상품명
+ * @apiParam (Body) {Number} price 가격
+ *
+ * @apiSuccessExample {json} SUCCESS
+ * HTTP/1.1 200
+ */
