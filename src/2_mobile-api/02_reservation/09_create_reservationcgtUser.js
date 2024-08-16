de4eb48fffb3045feb488168870ed275138b6dbc
gyj <jinyjgo@gmail.com>
2024-08-16 17:02:48 +0900

    모바일> cgt예약목록,예약취소,예약/ 비밀번호변경

--- src/2_mobile-api/02_reservation/09_create_reservationcgtUser.js
+++ src/2_mobile-api/02_reservation/09_create_reservationcgtUser.js
@@ -6,7 +6,7 @@
  *
  * @apiVersion 1.0.0
  * @apiName admin_createCgtmember
- * @apiGroup 1. Admin API > 05. 강사
+ * @apiGroup 2. Mobile API > 02. 예약
  *
  * @apiExample {curl} REQUEST
  * curl -i -X PUT 'http://localhost:8080/admin/v1/teachers/reservationCgt' \
