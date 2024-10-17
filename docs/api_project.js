9a21982fd440899719aee13675153f6cd331153c
ilovecorea <ilovecorea@gmail.com>
2024-10-17 18:27:46 +0900

    1

--- docs/api_project.js
+++ docs/api_project.js
@@ -17,7 +17,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2024-10-15T05:24:55.191Z",
+    "time": "2024-10-17T09:27:37.642Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
